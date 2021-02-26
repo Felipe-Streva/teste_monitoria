72afb23b21c03092f77abe3252c3bb3c6afa8ea7
Felipe Streva <fstreva@poli.ufrj.br>
2021-02-26 19:27:00 -0300

    aula 26/02

--- react_monitoria/src/Components/Button/Button.js
+++ react_monitoria/src/Components/Button/Button.js
@@ -3,9 +3,21 @@ import styles from './Button.module.css';
 
 export default class Button extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {disabled: false}
+        this.desabilitar = this.desabilitar.bind(this)
+    }
+
+    desabilitar(){
+        console.log('cliquei')
+        this.setState({disabled: true})
+        setTimeout(()=>{this.setState({disabled: false})},3000)
+    }
+
     render(){
         return (
-            <button className={styles.button} style={{width: this.props.largura}} onClick={this.props.onClick}>{this.props.children}</button>
+            <button className={styles.button} style={{width: this.props.largura}} disabled={this.state.disabled} onClick={this.desabilitar}>{this.props.children}</button>
         )
     }
 }
